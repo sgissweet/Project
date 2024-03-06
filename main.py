55254e347879c09198cba06566a3c36060c5f155
sgissweet <nitkamon.67908@gmail.com>
2024-03-06 17:50:47 +0700

    14

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ WriteARead = Controller()
 #create temporary instance
 Mo = Writer("Mozaza", "namchakeawpun", "12/05/2000")
 pintt = Reader("Pinttttt", "sawasdee", "01/01/2005")
-Controller.add_writer(Mo)
-Controller.add_reader(pintt)
-Controller.add_reader(Reader("Pangrum", "ehehe", "02/01/2005"))
-Controller.add_reader(Reader("Jueeen", "whippedcream", "12/11/2004"))
+WriteARead.add_writer(Mo)
+WriteARead.add_reader(pintt)
+WriteARead.add_reader(Reader("Pangrum", "ehehe", "02/01/2005"))
+WriteARead.add_reader(Reader("Jueeen", "whippedcream", "12/11/2004"))
 
 # Book (self,name,writer,tag_list,status,age_restricted,prologue,date_time):
 Book1 = Book("Shin_chan", Mo, ["kids", "comedy","crime"], "publishing", 7, "shin_chan_prologue")
@@ -30,17 +30,17 @@ Mo.add_writing_book_list(Book2)
 Chapter1_1 = Chapter("1", "first chapter of shinchan", "this is the first chapter of shinchan", "01/01/2020", 5)
 
 book_sale = BookPromotion("01/01/2021", 50, [])
-Controller.add_promotion(book_sale)
+WriteARead.add_promotion(book_sale)
 
 promotion_12_12 = CoinPromotion("01/01/2021", 40, "December")
 promotion_11_11 = CoinPromotion("01/01/2021", 20, "November")
-Controller.add_promotion(promotion_12_12)
-Controller.add_promotion(promotion_11_11)
+WriteARead.add_promotion(promotion_12_12)
+WriteARead.add_promotion(promotion_11_11)
 
 
 # now = datetime.now()
-# Controller.buy_coin("Pinttttt", (TrueMoneyWallet("0123456789")), "December", 5000)
-# Controller.buy_coin("Pinttttt", (OnlineBanking("0123456789")), "November", 100)
+# WriteARead.buy_coin("Pinttttt", (TrueMoneyWallet("0123456789")), "December", 5000)
+# WriteARead.buy_coin("Pinttttt", (OnlineBanking("0123456789")), "November", 100)
 
 #=====================================================================================================
 
@@ -50,33 +50,33 @@ app = FastAPI()
 def SignUp(username:str, password:str, birth_date: str):
     new_reader = Reader(username,password,birth_date)
     if isinstance(new_reader, Reader)==True:
-        Controller.add_reader(new_reader)
+        WriteARead.add_reader(new_reader)
         return {"User": "sign up success"}
     else : 
         return {"User": "please try again"}
 
 @app.get("/bookname", tags=['Search'])
 def searchBook(book_name:str):
-    return {"Book": Controller.search_book_by_name(book_name)}
+    return {"Book": WriteARead.search_book_by_name(book_name)}
 
 @app.get("/username", tags=['Search'])
 def SearchUser(username:str):
-     return {"username": Controller.search_user(username)}
+     return {"username": WriteARead.search_user(username)}
 
 @app.get("/get_coin_transaction", tags=['Coin Transaction'])
 def get_coin_transaction(username:str):
-    user = Controller.get_user_by_username(username)
+    user = WriteARead.get_user_by_username(username)
     return {"Coin Transaction" : user.show_coin_transaction()}
 
 @app.get("/get_my_coin", tags=['My Coin'])
 def get_my_coin(username:str):
-    user = Controller.get_user_by_username(username)
+    user = WriteARead.get_user_by_username(username)
     return {"Golden Coin balance" : user.golden_coin.balance, "Silver Coin balance" : user.show_silver_coin_list()}
 
 @app.post("/post_payment_method", tags=['Buy Coin'])
-def buy_coin(username:str, golden_coin_amount:int, payment_info: Annotated[str | None, Query(max_length = 10)], payment_method:str = Query("Payment Method", enum = Controller.payment_list, description ='Choose your payment method'), code: Optional[str] = None):
-    payment = Controller.create_payment_method(payment_method, payment_info)
-    Controller.buy_coin(username, payment, code, golden_coin_amount)  
+def buy_coin(username:str, golden_coin_amount:int, payment_info: Annotated[str | None, Query(max_length = 10)], payment_method:str = Query("Payment Method", enum = WriteARead.payment_list, description ='Choose your payment method'), code: Optional[str] = None):
+    payment = WriteARead.create_payment_method(payment_method, payment_info)
+    WriteARead.buy_coin(username, payment, code, golden_coin_amount)  
     return "Purchase successful, THANK YOU"
 
 @app.post("/Buy Chapter", tags=['chapter'])
