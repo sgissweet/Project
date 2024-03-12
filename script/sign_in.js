8aa0459b840e27b8fd25ea5b4f67b8b11b00ab54
sgissweet <nitkamon.67908@gmail.com>
2024-03-13 06:23:15 +0700

    final login/signup

--- script/sign_in.js
+++ script/sign_in.js
@@ -6,23 +6,27 @@ async function alert_success() {
     // console.log(response.data);
     console.log(response.data.response);
     console.log(response.data.role);
-
-    if(response.data.response == "log in successfully") {
-
-        window.location.href = 'homepage.html';
-
-        alert('เข้าสู่ระบบสำเร็จ!');
-        localStorage.setItem('login_username', username);
-        localStorage.setItem('login_role', response.data.role);
-
-        console.log(localStorage.getItem('login_username'));
-        console.log(localStorage.getItem('login_role'));
-
-    } else if(response.data.response == "wrong password") {
-        alert('เข้าสู่ระบบล้มเหลว! กรุณาตรวจสอบชื่อผู้ใช้และรหัสผ่านของคุณ');
-
+    if(password.length < 8) {
+        alert('รหัสมากกว่า 8 ตัวอักษรขึ้นไปจ้า');
     } else {
-        alert('เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง');
+        
+        if(response.data.response == "log in successfully") {
+
+            window.location.href = 'homepage.html';
+
+            alert('เข้าสู่ระบบสำเร็จ!');
+            localStorage.setItem('login_username', username);
+            localStorage.setItem('login_role', response.data.role);
+
+            console.log(localStorage.getItem('login_username'));
+            console.log(localStorage.getItem('login_role'));
+
+        } else if(response.data.response == "wrong password") {
+            alert('เข้าสู่ระบบล้มเหลว! กรุณาตรวจสอบชื่อผู้ใช้และรหัสผ่านของคุณ');
+
+        } else {
+            alert('เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง');
+        }
     }
     
 }
--- script/sign_up.js
+++ script/sign_up.js
@@ -11,29 +11,35 @@ async function alert_success() {
     const password = document.getElementById('password').value;
     const birth_date = document.getElementById('birth_date').value;
 
-    
-    axios.post("http://127.0.0.1:8000/sign_up", {
-        "username": username,
-        "password": password,
-        "birth_date": birth_date,
-        "role": role
-    })
-    .then((response) => {
-    console.log(response.data);
-
-    if(response.data == "Sign Up Successful") {
-        window.location.href = 'sign_in.html';
-        alert('ลงทะเบียนสำเร็จ!');
-
-    } else if(response.data == "username is already taken") {
-        alert('ชื่อนี้ถูกใช้แล้ว');
-
-    } else {
-        alert('เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง');
+    if(password.length < 8) {
+        alert('ตั้งรหัสมากกว่า 8 ตัวอักษรขึ้นไปจ้า!');
+        window.location.reload(); 
     }
-
     
-    })
+    else {
+        
+        axios.post("http://127.0.0.1:8000/sign_up", {
+            "username": username,
+            "password": password,
+            "birth_date": birth_date,
+            "role": role
+        })
+        .then((response) => {
+        console.log(response.data);
+        
+        if(response.data == "Sign Up Successful") {
+            window.location.href = 'sign_in.html';
+            alert('ลงทะเบียนสำเร็จ!');
+    
+        } else if(response.data == "username is already taken") {
+            alert('ชื่อนี้ถูกใช้แล้ว');
+    
+        } else {
+            alert('เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง');
+        }
+    
+        })
+    }
 
     
     // console.log(response.data.response);
