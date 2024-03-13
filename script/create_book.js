9abd618534b5a8fcfd61e2ce00fc0805c2609038
sgissweet <nitkamon.67908@gmail.com>
2024-03-13 07:14:11 +0700

    create book done

--- script/create_book.js
+++ script/create_book.js
@@ -1,29 +1,49 @@
-const create_chap_data = {};
-
-    // book_name:str
-    // chapter_number:int
+const create_book_data = {};
     // name:str
-    // context: str
-    // cost : int
+    //  pseudonym:str
+    //  writer_name:str
+    //  genre: str
+    //  prologue: str
+    //  age_restricted: bool
+    //  status: str 
 
 // create_chap_data.book_name = localStorage.getItem('');
-localStorage.getItem("book_name_last");
+localStorage.getItem("login_username");
 
-create_chap_data.book_name = localStorage.getItem("book_name_last");
+// create_book_data.writer_name = localStorage.getItem("login_username");
+create_book_data.writer_name = "Mozaza";
 
+create_book_data.name = document.getElementById('book_name').value;
+create_book_data.pseudonym = document.getElementById('pseudonym').value;
+create_book_data.genre = document.getElementById('tag').value;
+create_book_data.prologue = document.getElementById('prologue').value;
+// create_book_data.age_restricted = document.getElementById('age_restricted').value;
+// create_book_data.status = document.getElementById('status').value;
+
+
+document.getElementById('status').addEventListener('change', function() {
+    create_book_data.status = this.value;
+});
+
+document.getElementById('age_restricted').addEventListener('change', function() {
+    create_book_data.age_restricted = this.value;
+});
 
 async function submit_create_book() {
-    create_chap_data.chapter_number = document.getElementById('chapter_number').value;
-    create_chap_data.name = document.getElementById('chapter_name').value;
-    create_chap_data.context = document.getElementById('context').value;
-    create_chap_data.cost = document.getElementById('cost').value;
+    create_book_data.name = document.getElementById('book_name').value;
+    create_book_data.pseudonym = document.getElementById('pseudonym').value;
+    create_book_data.genre = document.getElementById('tag').value;
+    create_book_data.prologue = document.getElementById('prologue').value;
+    // create_book_data.age_restricted = document.getElementById('age_restricted').value;
 
-    axios.post("http://127.0.0.1:8000/chapter", {
-        "book_name": create_chap_data.book_name,
-        "chapter_number": create_chap_data.chapter_number,
-        "name": create_chap_data.name,
-        "context": create_chap_data.context,
-        "cost": create_chap_data.cost
+    axios.post("http://127.0.0.1:8000/book", {
+        "name": create_book_data.name,
+        "pseudonym": create_book_data.pseudonym,
+        "writer_name": create_book_data.writer_name,
+        "genre": create_book_data.genre,
+        "prologue": create_book_data.prologue,
+        "age_restricted": create_book_data.age_restricted,
+        "status": create_book_data.status 
       })
       .then((response) => {
         console.log(response.data);
